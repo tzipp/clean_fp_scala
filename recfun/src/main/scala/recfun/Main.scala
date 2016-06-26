fb2bfb4311354f528a688347a74515a34b2bf43a
tzipp <trfarrell@optimum.net>
2016-06-26 15:21:20 -0400

    Pascals triangle

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -13,7 +13,12 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(c: Int, r: Int): Int = {
+      if (c == 0) 1
+      else if (c == r) 1
+      else if (c > 0 && c < r) r
+      else 0 // out of bounds
+    }
   
   /**
    * Exercise 2
@@ -24,4 +29,4 @@ object Main {
    * Exercise 3
    */
     def countChange(money: Int, coins: List[Int]): Int = ???
-  }
+}
