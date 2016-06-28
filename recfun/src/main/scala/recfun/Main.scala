fd09f62c27fc36fc58ee0519ecd96a7811be2b64
tzipp <trfarrell@optimum.net>
2016-06-28 19:10:40 -0400

    Balance parenthesis.

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -11,22 +11,37 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = {
-      if (c == 0) 1
-      else if (c == r) 1
-      else if (c > 0 && c < r) r
-      else 0 // out of bounds
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0) 1
+    else if (c == r) 1
+    else if (c > 0 && c < r) r
+    else 0 // out of bounds
+  }
+
+  /**
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = {
+    def loop(lst: List[Char], acc: Int): Int = {
+      if (lst.isEmpty) acc
+      else if (acc < 0) - 1
+      else {
+        val hd = lst.head
+        val tl = lst.tail
+        if (hd == '(') loop(tl, acc+1)
+        else if (hd == ')') loop(tl, acc-1)
+        else loop(tl, acc)
+      }
     }
-  
+    val result = loop(chars, 0)
+    if (result == 0) true
+    else false
+  }
+
   /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = ???
 }
